9a72138c53455f9c14f95892056097cba32930a0
thegostisdead <thegostisdead@gmail.com>
2024-07-12 10:22:21 +0200

    add exit command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,11 +4,33 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func parseLine(line string) {
 	// Remove newline character
 	line = line[:len(line)-1]
+
+	if strings.HasPrefix(line, "exit") {
+		// get exit args
+		args := strings.Split(line, " ")
+		if len(args) <= 1 {
+			fmt.Println("exit: need 1 argument")
+			return
+		}
+
+		// get exit code
+		rawCode := args[1]
+		res, err := strconv.Atoi(rawCode)
+		if err != nil {
+			fmt.Println("exit: argument must be an integer")
+			return
+		}
+		os.Exit(res)
+
+	}
+
 	fmt.Println(line + ": command not found")
 }
 
